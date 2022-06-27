540210044149ec164759d7fb5d9aaa2d8a8a180c
isen <yichaotang@isendeMacBook-Pro.local>
2022-06-27 22:06:05 +0800

    tree init

--- src/views/tree/TreeView.vue
+++ src/views/tree/TreeView.vue
@@ -1,11 +1,43 @@
 <template>
   <div>
-    Tree
+    学生推荐关系图
+    <el-tree :data="data" />
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 
+const data = ref([
+  {
+    label: 'isen',
+    children: [
+      {
+        label: 'lucy',
+        children: [
+          {
+            label: 'lily',
+          },
+          {
+            label: 'tom',
+          },
+        ],
+      },
+      {
+        label: 'tomy',
+        children: [
+          {
+            label: 'jerry',
+          },
+          {
+            label: 'anna',
+          },
+        ],
+      },
+    ],
+  },
+
+]);
 </script>
 
 <style lang="scss" scoped>
