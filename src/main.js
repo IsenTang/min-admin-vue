5670ef0692725e2a70c0f2df14a8ef0d16c3547e
Isen Tang <3104255643@qq.com>
2022-06-28 20:42:48 +0800

    懒加载

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import ElementPlus from 'element-plus';
 import moment from 'moment';
+import VueLazyLoad from 'vue3-lazyload';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -9,7 +10,8 @@ import 'element-plus/dist/index.css';
 // ? Vue3.0 不再支持过滤器,建议使用计算属性/方法
 // ? 也可以使用以下方法
 
-const app = createApp(App).use(store).use(router).use(ElementPlus);
+const app = createApp(App).use(store).use(router).use(ElementPlus)
+  .use(VueLazyLoad);
 app.config.globalProperties.$filters = {
   formatTime(v) {
     return moment(v).format('YYYY-MM-DD HH:mm:ss');
--- src/views/lazy/LazyView.vue
+++ src/views/lazy/LazyView.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
     懒加载图片
+    <div class="container" />
+    <img
+      v-lazy="{ src: url}"
+      alt=""
+    >
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 
+const url = ref('https://images.unsplash.com/photo-1527960669566-f882ba85a4c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXdlc29tZSUyMHBpY3xlbnwwfHwwfHw%3D&w=1000&q=80');
 </script>
 
 <style lang="scss" scoped>
-
+  .container{
+    height: 100vh;
+    background-color: aquamarine;
+  }
 </style>
